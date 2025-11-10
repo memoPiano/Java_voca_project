6ac37a7e9bd2b6ee47cb64ccc82373852cfb50b7
어지수 <djwltn05@gmaile.com>
2025-11-10 22:57:06 +0900

    학번이름 추가

--- src/Main/MainMenu.java
+++ src/Main/MainMenu.java
@@ -3,5 +3,7 @@ package Main;
 public class MainMenu {
     public static void main(String[] args) {
         System.out.println("202411235 강동훈");
+        System.out.println("202510998 어지수");
+        
     }
 }
