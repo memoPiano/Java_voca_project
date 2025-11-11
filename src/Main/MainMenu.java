b297724ced3150cf24b33504149ae31693512cd7
Akimoto Tatsumi <tatsumi814rainbow@gmail.com>
2025-11-11 15:16:09 +0900

    Update MainMenu.java

--- src/Main/MainMenu.java
+++ src/Main/MainMenu.java
@@ -4,7 +4,7 @@ public class MainMenu {
     public static void main(String[] args) {
         System.out.println("202411235 강동훈");
         System.out.println("202510998 어지수");
-        System.out.println("202314517 아키모토타츠미);
+        System.out.println("202314517 아키모토타츠미");
         
     }
 }
