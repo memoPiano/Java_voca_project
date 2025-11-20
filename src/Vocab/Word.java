1b1c0f3b8a07bee31210f054c3aa878124edec0d
Akimoto Tatsumi <tatsumi814rainbow@gmail.com>
2025-11-20 14:12:07 +0900

    Update Word.java
    예문 리스트 추가

--- src/Vocab/Word.java
+++ src/Vocab/Word.java
@@ -31,6 +31,22 @@ public class Word {
         wrong_number++;
     }
 
+    // 예문을 저장하는 리스트
+// 한 단어에 여러 개의 예문을 추가할 수 있기 때문에 ArrayList 사용
+    private ArrayList<String> examples = new ArrayList<>();
+
+    // 예문 리스트를 반환하는 메소드
+// 다른 클래스에서 단어의 예문들을 확인할 때 사용
+    public ArrayList<String> getExamples() {
+        return examples;
+    }
+
+    // 새로운 예문을 추가하는 메소드
+// 단어 학습 시 예문을 계속 늘릴 수 있도록 설계됨
+    public void addExample(String example) {
+        examples.add(example);
+    }
+
     @Override
     public String toString() {
         return eng + " : " + kors;
